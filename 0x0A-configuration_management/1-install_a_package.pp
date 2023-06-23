3620b925d89879472b8f4934459bafd08e9d6e0b
Titisunflower <henuwz@gmail.com>
2023-06-23 06:03:05 -0700

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+  require => Package['python3-pip'],
 }
