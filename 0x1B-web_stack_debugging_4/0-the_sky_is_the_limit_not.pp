2929993d0cae39de4d784f834d8cafca5a4f2d86
Titisunflower <henuwz@gmail.com>
2023-08-17 15:24:11 -0700

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,6 @@
-# Increases the amount of traffic an Nginx server can handle.
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+# Script to increase the number of requests Nginx web server can take
+exec { 'Increase Nginx ULIMIT':
+  command => 'sudo sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0, 1]
 }
